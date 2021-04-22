0f1d29c02ee6238a16468bbb1b67967810ef197f
s.dalessio8 <s.dalessio8@studenti.unisa.it>
2021-04-22 23:57:38 +0200

    First version of the script

---
+++ weboff.sh
@@ -0,0 +1,39 @@
+# Author: sim_1e097cd
+
+if [ "$#" -gt "0" ]
+then 
+	if [ $1 == '-off' ] 
+	then
+		var=$(ls /dev | grep 'video[0-99]')
+		list=($var)
+		for elem in ${list[*]} 
+		do
+			mv /dev/$elem /dev/weboff${elem: -1}
+		done
+		if [ "$#" -gt "1" ]
+		then 
+			if [ "$2" -gt "0" ] && [ "$2" -lt "1000" ] 
+			then
+				sleep $2
+				for elem in ${list[*]} 
+				do
+					mv /dev/weboff${elem: -1} /dev/$elem
+				done
+			else 
+				echo 'usage ./weboff -off [seconds]'
+			fi
+		fi
+	elif [ $1 == '-on' ] 
+	then
+		var=$(ls /dev | grep 'weboff[0-99]')
+		list=($var)
+		for elem in ${list[*]} 
+		do
+			mv /dev/$elem /dev/video${elem: -1}
+		done
+	else
+		echo 'usage ./weboff -off [seconds]'
+	fi
+else
+	echo 'usage ./weboff -off [seconds]'
+fi
